a28d1f647b4bb77b845f0fcc6b79943b985a00f7
Dominik Molenda <molenda.dominik@yahoo.com>
2025-04-12 18:15:43 +0200

    account page fix

--- task-mgmt/app/account/page.js
+++ task-mgmt/app/account/page.js
@@ -50,33 +50,29 @@ export default function AccountPage() {
     <>
       {user ? (
         <div className="container d-flex flex-grow-1 main">
-            <div className="user_page my-auto main_wrapper d-flex flex-column align-items-center mx-auto border_custom">
-            {loading ? (<><h2>loading...</h2></>) : (
-              <>
-                <div className="stats d-flex justify-content-between my-4 w-75">
-                  <div className="stat_info border_custom button_style">open tasks: {openTasksNumber}</div>
-                  <div className="stat_info border_custom button_style">closed tasks: {closedTasksNumber}</div>
-                  <div className="stat_info border_custom button_style">all tasks: {allTasksNumber}</div>
-                  <div className="stat_info border_custom button_style">current lvl: {user.current_lvl}</div>
-                </div>
-                <div>
-                  <div className="d-flex align-items-center my-4">
-                    {/* <img src="assets/person_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
-                    <p className="border_custom button_style mb-0">name: {user.name}</p>
-                  </div>
-                  <div className="d-flex align-items-center my-4">
-                    {/* <img src="assets/mail_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
-                    <p className="border_custom button_style mb-0">email: {user.email}</p>
-                  </div>
-                </div>
-                <div >
-                  <Link href='/account/edit' style={{ textDecoration: "none" }}>
-                    <button className="border_custom button_style">edit</button>
-                  </Link>
-                </div>
-              </>
-              )}
+          <div className="user_page my-auto main_wrapper d-flex flex-column align-items-center mx-auto border_custom">
+            <div className="stats d-flex justify-content-between my-4 w-75">
+              <div className="stat_info border_custom button_style">open tasks: {openTasksNumber}</div>
+              <div className="stat_info border_custom button_style">closed tasks: {closedTasksNumber}</div>
+              <div className="stat_info border_custom button_style">all tasks: {allTasksNumber}</div>
+              <div className="stat_info border_custom button_style">current lvl: {user.current_lvl}</div>
             </div>
+            <div>
+              <div className="d-flex align-items-center my-4">
+                {/* <img src="assets/person_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
+                <p className="border_custom button_style mb-0">name: {user.name}</p>
+              </div>
+              <div className="d-flex align-items-center my-4">
+                {/* <img src="assets/mail_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
+                <p className="border_custom button_style mb-0">email: {user.email}</p>
+              </div>
+            </div>
+            <div >
+              <Link href='/account/edit' style={{ textDecoration: "none" }}>
+                <button className="border_custom button_style">edit</button>
+              </Link>
+            </div>
+          </div>
         </div>
       ) : (
         <h1>Error 418 I&apos;m a teapot</h1>
