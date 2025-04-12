2d6ea4513db53af7bbfd83cd1cf25ee3ede162a9
Kirill Rodin <kirr.rodin@gmail.com>
2025-04-12 17:45:55 +0200

    profile up

--- task-mgmt/app/account/page.js
+++ task-mgmt/app/account/page.js
@@ -47,29 +47,27 @@ export default function AccountPage() {
   }
 
   return (
-    <div>
+    <div className="container d-flex flex-grow-1 main">
       {user ? (
-        <div className="container d-flex flex-grow-1 main">
-          <div className="user_page my-auto main_wrapper d-flex flex-column align-items-center mx-auto border_custom">
-            <div className="stats d-flex justify-content-between my-4 w-75">
-              <div className="stat_info border_custom button_style">open tasks: {openTasksNumber}</div>
-              <div className="stat_info border_custom button_style">closed tasks: {closedTasksNumber}</div>
-              <div className="stat_info border_custom button_style">all tasks: {allTasksNumber}</div>
-              <div className="stat_info border_custom button_style">current lvl: {user.current_lvl}</div>
+        <div className="user_page my-auto main_wrapper d-flex flex-column align-items-center mx-auto border_custom">
+          <div className="stats d-flex justify-content-between my-4 w-75">
+            <div className="stat_info border_custom button_style">open tasks: {openTasksNumber}</div>
+            <div className="stat_info border_custom button_style">closed tasks: {closedTasksNumber}</div>
+            <div className="stat_info border_custom button_style">all tasks: {allTasksNumber}</div>
+            <div className="stat_info border_custom button_style">current lvl: {user.current_lvl}</div>
+          </div>
+          <div>
+            <div className="d-flex align-items-center my-4">
+              {/* <img src="assets/person_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
+              <p className="border_custom button_style mb-0">name: {user.name}</p>
             </div>
-            <div>
-              <div className="d-flex align-items-center my-4">
-                {/* <img src="assets/person_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
-                <p className="border_custom button_style mb-0">name: {user.name}</p>
-              </div>
-              <div className="d-flex align-items-center my-4">
-                {/* <img src="assets/mail_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
-                <p className="border_custom button_style mb-0">email: {user.email}</p>
-              </div>
+            <div className="d-flex align-items-center my-4">
+              {/* <img src="assets/mail_64dp_EBD478_FILL0_wght200_GRAD0_opsz48.svg" alt="" className="me-3"> */}
+              <p className="border_custom button_style mb-0">email: {user.email}</p>
             </div>
-            <div >
-              <button className="border_custom button_style">edit</button>
-            </div>
+          </div>
+          <div >
+            <button className="border_custom button_style">edit</button>
           </div>
         </div>
       ) : (
