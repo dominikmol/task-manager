eeda4b9520e624ea3086b9547ca88d02840ab06a
Dominik Molenda <molenda.dominik@yahoo.com>
2025-04-14 12:32:07 +0200

    select update

--- task-mgmt/app/tasks/new/page.js
+++ task-mgmt/app/tasks/new/page.js
@@ -58,11 +58,11 @@ export default function NewTaskPage() {
                                     <input className="border_custom login_form" type="date" name='date' />
                                     <select name="taskLevel" className="border_custom login_form" defaultValue="">
                                         <option value="" disabled hidden>task LVL</option>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
+                                        <option value="1">very easy</option>
+                                        <option value="2">easy</option>
+                                        <option value="3">medium</option>
+                                        <option value="4">hard</option>
+                                        <option value="5">very hard</option>
                                     </select>
                                 </div>
                                 <div className="w-100"></div>
